8f2cc353a24cd488a34d36fb3f40753d54f81aad
Jaime <jmr.877@gmail.com>
2022-12-14 20:50:29 +0100

    1 commit(Página de creación de cuenta)

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    //
+    public function index()
+    {
+        return view('auth.register');
+    }
+
+    public function store(Request $request)
+    {
+
+        //Modificar Request
+
+        $request->request->add(['username' => Str::slug($request->username)]);
+
+        // Validación de cammpos formulario
+
+        $this->validate($request, [
+            'name'=>'required|max:40',
+            'username'=>'required|unique:users|min:3|max:20',
+            'email'=>'required|unique:users|email|max:60',
+            'password'=>'required|confirmed|min:6',
+            
+        ]); 
+
+        // Creación de usuario, datos que guardamos en base de datos Devstagram en la tabla users.
+        // Además, la contraseña se guarda hasheada.
+        User::create([
+            'name' => $request->name,
+            'username' =>  $request->username,
+            'email' => $request->email,
+            'password' => Hash::make( $request->password)
+        ]);
+
+        // Redireccionar al usuario cuando ha creado una cuenta.
+
+        return redirect()->route('post.index');
+
+
+    }
+}
---
+++ resources/views/contacto.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('titulo')
+    Datos de contacto
+@endsection
+
+@section('contenido')
+    Contenido de la página
+@endsection
